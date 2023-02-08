fb7d5230aedfaa11447c53517869d46287206c3d
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-02-08 16:39:17 -0500

    Testing locations in github action

---
+++ .github/workflows/locations.ps1
@@ -0,0 +1,4 @@
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+Install-Module powershell-yaml -ErrorAction Stop
+$win11642009 = Convertfrom-Yaml (Get-Content "config/win11-64-2009.yaml" -raw)
+Write-Host "::set-output name=matrix::$($win11642009.azure.locations | ConvertTo-JSON -Compress))"
