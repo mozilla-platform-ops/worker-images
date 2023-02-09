882a2b484b8abcdf760e5bdc3e3520c9b240800d
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-02-09 09:15:10 -0500

    try again

--- .github/workflows/locations.ps1
+++ .github/workflows/locations.ps1
@@ -1,4 +1,10 @@
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module powershell-yaml -ErrorAction Stop
 $win11642009 = Convertfrom-Yaml (Get-Content "config/win11-64-2009.yaml" -raw)
-Write-Host "::set-output name=matrix::$($win11642009.azure | ConvertTo-JSON -Compress))"
+$locations = @()
+$win11642009.azure.locations | ForEach-Object {
+    $locations += @{
+        location = $_
+    }
+}
+Write-Host "::set-output name=matrix::$($locations | ConvertTo-JSON -Compress))"
