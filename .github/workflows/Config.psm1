b4a2cdfb306c4767aa3c52a095f55c46f3a4023d
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-03-06 16:11:38 -0500

    willthiswork win11-64-2009-alpha yeehaw

---
+++ .github/workflows/Config.psm1
@@ -0,0 +1,23 @@
+function Set-WorkerImageOutput {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $CommitMessage
+    )
+    
+    Set-PSRepository PSGallery -InstallationPolicy Trusted
+    Install-Module powershell-yaml -ErrorAction Stop
+    $Commit = (ConvertFrom-Json $CommitMessage) -split " "
+    #$Commit = (ConvertFrom-Json '${{toJSON(github.event.head_commit.message)}}') -Split " "
+    $Key = $Commit | Foreach-object {
+        if ($_ -match "-") {
+            $_
+        }
+    }
+    $YAML = Convertfrom-Yaml (Get-Content "config/$key.yaml" -raw)
+    $locations = ($YAML.azure.locations | ConvertTo-Json -Compress)
+    Write-Output "LOCATIONS=$locations" >> $ENV:GITHUB_OUTPUT
+}
+
+Export-ModuleMember -Function *
