5c88429ff1b1e25e4bf569ad66672646ef60e1e8
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-03-28 09:18:15 -0400

    Parse commit file

--- .github/workflows/tools.ps1
+++ .github/workflows/tools.ps1
@@ -6,17 +6,25 @@ function Set-WorkerImageOutput {
         $CommitMessage
     )
     
-    Set-PSRepository PSGallery -InstallationPolicy Trusted
-    Install-Module powershell-yaml -ErrorAction Stop
-    $Commit = (ConvertFrom-Json $CommitMessage) -split " "
-    #$Commit = (ConvertFrom-Json '${{toJSON(github.event.head_commit.message)}}') -Split " "
-    $Key = $Commit | Foreach-object {
-        if ($_ -match "-") {
-            $_
+    $Commit = ConvertFrom-Json $CommitMessage
+    ## Handle the pools and pluck them out
+    $keys_index = $commit.IndexOf("keys:")
+    $keys = if ($keys_index -ne -1) {
+        $keys_value = $commit.Substring($keys_index + 5).Trim()
+        if ($keys_value -match ",") {
+            $keys_array = $keys_value.Split(",")
+            foreach ($key in $keys_array) {
+                $key.Trim()
+            }
+        }
+        else {
+            $keys_value.Trim()
         }
     }
-    $YAML = Convertfrom-Yaml (Get-Content "config/$key.yaml" -raw)
-    $locations = ($YAML.azure.locations | ConvertTo-Json -Compress)
-    Write-Output "LOCATIONS=$locations" >> $ENV:GITHUB_OUTPUT
-    Write-Output "KEY=$Key" >> $ENV:GITHUB_OUTPUT
+    Foreach ($key in $keys) {
+        $YAML = Convertfrom-Yaml (Get-Content "config/$key.yaml" -raw)
+        $locations = ($YAML.azure.locations | ConvertTo-Json -Compress)
+        Write-Output "LOCATIONS=$locations" >> $ENV:GITHUB_OUTPUT
+        Write-Output "KEY=$Key" >> $ENV:GITHUB_OUTPUT
+    }
 }
