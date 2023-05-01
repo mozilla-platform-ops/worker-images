e47e2d64b45b34e750b6a3cf498b500470e346f8
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-05-01 16:13:12 -0400

    Build win11 in devtest
    include environments: production
    
    include keys: win11-64-2009-alpha

--- .github/workflows/tools.ps1
+++ .github/workflows/tools.ps1
@@ -38,7 +38,19 @@ function New-WorkerImage {
         $Location,
 
         [String]
-        $CommitMessage
+        $CommitMessage,
+
+        [String]
+        $Client_ID,
+
+        [String]
+        $Client_Secret,
+
+        [String]
+        $Subscription_ID,
+
+        [String]
+        $Tenant_ID
     )
 
     Set-PSRepository PSGallery -InstallationPolicy Trusted
@@ -73,11 +85,11 @@ function New-WorkerImage {
         $ENV:PKR_VAR_source_organization = $YAML.vm.tags["sourceOrganization"]
         $ENV:PKR_VAR_deployment_id = $YAML.vm.tags["deploymentId"]
         $ENV:PKR_VAR_bootstrap_script = $YAML.azure["bootstrapscript"]
-        $ENV:PKR_VAR_client_id = $ENV:client_id
+        $ENV:PKR_VAR_client_id = $Client_ID
         $ENV:PKR_VAR_temp_resource_group_name = ('{0}-{1}-{2}-{3}-pkrtmp' -f $YAML.vm.tags["worker_pool_id"], $ENV:PKR_VAR_location, $YAML.vm.tags["deploymentId"], (Get-Random -Maximum 999))
-        $ENV:PKR_VAR_tenant_id = $ENV:tenant_id
-        $ENV:PKR_VAR_subscription_id = $ENV:subscription_id
-        $ENV:PKR_VAR_client_secret = $ENV:client_secret
+        $ENV:PKR_VAR_tenant_id = $Tenant_ID
+        $ENV:PKR_VAR_subscription_id = $Subscription_ID
+        $ENV:PKR_VAR_client_secret = $Client_Secret
         $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-alpha' -f $YAML.vm.tags["worker_pool_id"], $ENV:PKR_VAR_image_sku)
         Write-Host "Building $($ENV:PKR_VAR_managed_image_name)"
         $ENV:PKR_VAR_image_version = $ImageVersion
