dfe3046d2f7bc905154743c88863968dd07fb35e
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-09-21 17:44:24 -0400

    Install powershell yaml Fix filter

--- scripts/windows/CustomFunctions/Bootstrap/Public/Get-RoninTest.ps1
+++ scripts/windows/CustomFunctions/Bootstrap/Public/Get-RoninTest.ps1
@@ -5,12 +5,17 @@ function Get-RoninTest {
         $Key
     )
     
+    ## Install Powershell YAML module
+    Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Install-Module -Name "PowerShell-Yaml" -Repository PSGallery -Force
+
     ## Get just the tests that are defined in the config
     $Hiera = Convertfrom-Yaml (Get-Content -Path "C:\ronin\data\roles\$key.yaml" -Raw)
     
     ## Loop through the tests based on which ones were selected
     $hiera.tests | ForEach-Object {
         $name = $psitem
-        Get-ChildItem -Path "C:/Tests/*" -Filter $name
+        Get-ChildItem -Path "C:/Tests/*" -Filter "*$name*"
     }
 }
