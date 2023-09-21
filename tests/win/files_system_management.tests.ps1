6025384b19c6260c816f444f2fae374772951cad
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-09-21 13:17:03 -0400

    Move stuff around

--- tests/win/files_system_management.tests.ps1
+++ tests/win/files_system_management.tests.ps1
@@ -5,7 +5,7 @@ Describe "File System Management" {
     It "Disable Last Acccess" {
         Get-ItemPropertyValue "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "NtfsDisableLastAccessUpdate" | Should -Be 2147483649
     }
-    It "Long paths enabled" {
+    It "Long paths enabled" -Skip {
         Get-ItemPropertyValue "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" | Should -Be 1
     }
     It "PageFile is set to Y" -Skip {
--- tests/win/windows_worker_runner.tests.ps1
+++ tests/win/windows_worker_runner.tests.ps1
@@ -1,12 +1,9 @@
 Describe "Windows Worker Runner" {
-    BeforeAll {
-        $worker_runner_service = Get-Service "worker-runner"
-    }
     It "Custom NSSM exists" {
         Test-Path "C:\nssm\nssm-2.24\win64\nssm.exe" | Should -Be $true
     }
     It "Windows Service Exists" {
-        $worker_runner_service | Should -Not -Be $null
+        Get-Service "worker-runner" | Should -Not -Be $null
     }
     It "Worker runner directory exists" {
         Test-Path "C:\worker-runner" | Should -Be $true
