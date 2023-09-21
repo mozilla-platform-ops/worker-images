612b03e6d84e2a243384c03eb396e0b9d1bc4328
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-09-21 18:41:42 -0400

    Clean up the way tests are run

--- scripts/windows/CustomFunctions/Bootstrap/Public/Invoke-RoninTest.ps1
+++ scripts/windows/CustomFunctions/Bootstrap/Public/Invoke-RoninTest.ps1
@@ -1,33 +1,31 @@
 Function Invoke-RoninTest {
     [CmdletBinding()]
     param (
-        [Parameter()]
-        [String]
-        $Test,
-
-        [String[]]
-        $Tags,
-
-        [String[]]
-        $ExcludeTag,
-
         [String]
         $Key = $ENV:base_image,
 
         [Switch]
         $PassThru
     )
-    $Container = New-PesterContainer -Path $test -Data @{
+    ## Grab the tests from hiera
+    $Hiera = Convertfrom-Yaml (Get-Content -Path "C:\ronin\data\roles\$key.yaml" -Raw)
+    ## Select the tests and pass through to pester
+    $tests = @($Hiera.Tests) | ForEach-Object {
+        $name = $psitem
+        Get-ChildItem -Path "C:/Tests/$name"
+    }
+    ## Build the container and pass in the hiera key
+    $Container = New-PesterContainer -Path $tests -Data @{
         File = "C:\ronin\data\roles\$Key.yaml"
     }
     $config = New-PesterConfiguration
     $config.Run.Container = $Container
-    $config.Filter.Tag = $Tags
+    #$config.Filter.Tag = $Tags
     $config.TestResult.Enabled = $true
     $config.Output.Verbosity = "Detailed"
-    if ($ExcludeTag) {
-        $config.Filter.ExcludeTag = $ExcludeTag
-    }
+    #if ($ExcludeTag) {
+    #    $config.Filter.ExcludeTag = $ExcludeTag
+    #}
     if ($PassThru) {
         $config.Run.Passthru = $true
     }
