50bfa71cd992639f88231de0181049ef1a4f7c8d
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-10-05 17:40:44 -0400

    restore original copy

--- scripts/windows/CustomFunctions/Bootstrap/Public/Invoke-RoninTest.ps1
+++ scripts/windows/CustomFunctions/Bootstrap/Public/Invoke-RoninTest.ps1
@@ -1,47 +1,28 @@
 Function Invoke-RoninTest {
     [CmdletBinding()]
     param (
+        [Parameter()]
         [String]
-        $Role,
+        $Test,
 
-        [String]
-        $Config,
+        [String[]]
+        $Tags,
+
+        [String[]]
+        $ExcludeTag,
 
         [Switch]
         $PassThru
     )
-    ## Grab the tests from hiera
-    $Hiera = Convertfrom-Yaml (Get-Content -Path "C:\ronin\data\roles\$Role.yaml" -Raw)
-    $Config_tests = Convertfrom-Yaml (Get-Content -Path "C:\Config\$Config.yaml" -Raw)
-    if ($null -eq $Hiera) {
-        Write-host "Unable to find hiera key lookup $Role"
-        exit 1
-    }
-    if ($null -eq $Config_tests) {
-        Write-host "Unable to find hiera key lookup $Config"
-        exit 1
-    }
-    ## Select the tests and pass through to pester
-    $tests = foreach ($t in $Config_tests.tests) {
-        Get-ChildItem -Path "C:/Tests/$t"
-    }
-    ## Check the output of $tests to make sure the contents are there
-    if ($null -eq $tests) {
-        Write-host "Unable to select tests based on $config lookup"
-        exit 1
-    }
-    ## Build the container and pass in the hiera key
-    $Container = New-PesterContainer -Path $tests.FullName -Data @{
-        File = "C:\ronin\data\roles\$Role.yaml"
-    }
+    $Container = New-PesterContainer -Path $test
     $config = New-PesterConfiguration
     $config.Run.Container = $Container
-    #$config.Filter.Tag = $Tags
+    $config.Filter.Tag = $Tags
     $config.TestResult.Enabled = $true
     $config.Output.Verbosity = "Detailed"
-    #if ($ExcludeTag) {
-    #    $config.Filter.ExcludeTag = $ExcludeTag
-    #}
+    if ($ExcludeTag) {
+        $config.Filter.ExcludeTag = $ExcludeTag
+    }
     if ($PassThru) {
         $config.Run.Passthru = $true
     }
