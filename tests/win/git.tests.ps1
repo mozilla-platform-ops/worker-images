c196b377f99635fa764c783f3ac5d34a5696206b
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-10-17 16:42:15 -0400

    Use hiera

--- tests/win/git.tests.ps1
+++ tests/win/git.tests.ps1
@@ -1,14 +1,24 @@
+Param(
+    [String]
+    $File
+)
+
+BeforeDiscovery {
+    $Hiera = Get-HieraRoleData -Path $File
+}
+
 Describe "Git" {
     BeforeAll {
         $Git = Get-InstalledSoftware | Where-Object {
             $PSItem.DisplayName -match "Git"
         }
+        $ExpectedSoftwareVersion = [Version]($Hiera["win-worker"].git.version)
     }
     It "Git is installed" {
         $Git.DisplayName | Should -Not -Be $null
     }
 
-    It "Git Version is 2.36.1" {
-        $Git.DisplayVersion | Should -Be "2.37.3"
+    It "Git Version is the same" {
+        $Git.DisplayVersion | Should -Be $ExpectedSoftwareVersion
     }
 }
