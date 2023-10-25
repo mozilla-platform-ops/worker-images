9903ef01acbe462e6ffa08fc5fce51fbdf03e268
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-10-25 08:59:09 -0400

    Rollback checks for winsdk

--- tests/win/microsoft_tools_tester.tests.ps1
+++ tests/win/microsoft_tools_tester.tests.ps1
@@ -20,7 +20,7 @@ Describe "Microsoft Tools - Tester" {
     It "<_.DisplayName> is 10.1.19041.685" -ForEach @(
         Show-Win10SDK
     ) {
-        $PSItem.DisplayVersion | Should -Be "10.1.22621.1778"
+        $PSItem.DisplayVersion | Should -Be "10.1.19041.685"
     }
     It "<_.DisplayName> is installed" -ForEach @(
         Show-WinDotNet48
@@ -30,7 +30,7 @@ Describe "Microsoft Tools - Tester" {
     It "<_.DisplayName> is version 4.8.04084" -ForEach @(
         Show-WinDotNet48
     ) {
-        $_.DisplayVersion | Should -Be "4.8.09032"
+        $_.DisplayVersion | Should -Be "4.8.04084"
     }
     It "<_.DisplayName> is installed" -ForEach @(
         Show-vcc2019
