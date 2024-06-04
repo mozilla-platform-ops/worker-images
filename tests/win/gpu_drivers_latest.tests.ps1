389c942f71faf09af14caeff51dd365eed78422b
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-06-04 13:41:25 -0400

    Add latest gpu drivers

---
+++ tests/win/gpu_drivers_latest.tests.ps1
@@ -0,0 +1,17 @@
+Param(
+    [String]
+    $File
+)
+
+BeforeDiscovery {
+    $Hiera = Get-HieraRoleData -Path $File
+}
+
+Describe "Nvidia GPU Downloaded" {
+    BeforeAll {
+        $gpu = ($Hiera.'win-worker'.'gpu-latest'.name)
+    }
+    It "Nvidia GPU Drivers are downloaded" {
+        Test-Path "C:\$gpu" | Should -Be $true
+    }
+}
