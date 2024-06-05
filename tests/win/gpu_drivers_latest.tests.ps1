8078b1dc1fc40fa5d155e6a3867ddf9b287e0d0a
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-06-05 17:06:01 -0400

    Downloading to temp dir not c:\

--- tests/win/gpu_drivers_latest.tests.ps1
+++ tests/win/gpu_drivers_latest.tests.ps1
@@ -12,6 +12,6 @@ Describe "Nvidia GPU Downloaded" {
         $gpu = ($Hiera.'win-worker'.'gpu-latest'.name)
     }
     It "Nvidia GPU Drivers are downloaded" {
-        Test-Path "C:\538.15_grid_win10_win11_server2019_server2022_dch_64bit_international_azure_swl.exe" | Should -Be $true
+        Test-Path "$systemdrive\Windows\Temp\$($gpu).exe" | Should -Be $true
     }
 }
