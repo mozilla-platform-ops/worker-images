26ec810acf47152c0949c81f1261a02c7925d73f
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-07-11 13:49:37 -0400

    test: :white_check_mark: Add tests for checking the version of taskcluster binaries

--- tests/win/windows_worker_runner.tests.ps1
+++ tests/win/windows_worker_runner.tests.ps1
@@ -10,6 +10,10 @@ BeforeDiscovery {
 Describe "Taskcluster" {
     BeforeAll {
         $nssm = ($Hiera.'win-worker'.nssm.version)
+        $generic_worker_version = ($Hiera.'win-worker'.generic_worker.version)
+        $worker_runner_version = ($Hiera.'win-worker'.taskcluster.worker_runner.version)
+        $proxy_version = ($Hiera.'win-worker'.taskcluster.proxy.version)
+        $livelog_version = ($Hiera.'win-worker'.taskcluster.livelog.version)
     }
     Context "Non-Sucking Service Manager" {
         It "NSSM is installed" {
@@ -18,8 +22,49 @@ Describe "Taskcluster" {
         It "NSSM Windows Service Exists" {
             Get-Service "worker-runner" | Should -Not -Be $null
         }
-        #It "Worker runner directory exists" {
-        #    Test-Path "C:\worker-runner" | Should -Be $true
-        #}
+    }
+    Context "Taskcluster directories" {
+        It "Generic Worker" {
+            Test-Path "C:\generic-worker" | Should -Be $true
+        }
+        It "Worker Runner" {
+            Test-Path "C:\worker-runner" | Should -Be $true
+        }
+    }
+    Context "Generic Worker" {
+        It "Generic Worker exists" {
+            Test-Path "C:\generic-worker\generic-worker.exe" | Should -Be $true
+        }
+        It "Generic Worker Version is correct" {
+            $version = Start-Process -FilePath "C:\generic-worker\generic-worker.exe" -ArgumentList "--short-version" -Wait -NoNewWindow
+            $version | Should -be $generic_worker_version
+        }
+    }
+    Context "Worker Runner" {
+        It "Worker Runner exists" {
+            Test-Path "C:\worker-runner\start-worker.exe" | Should -Be $true
+        }
+        It "Worker Runner Version is correct" {
+            $version = Start-Process -FilePath "C:\worker-runner\start-worker.exe" -ArgumentList "--short-version" -Wait -NoNewWindow
+            $version | Should -be $worker_runner_version
+        }
+    }
+    Context "Proxy" {
+        It "Proxy exists" {
+            Test-Path "C:\generic-worker\taskcluster-proxy.exe" | Should -Be $true
+        }
+        It "Proxy version is correct" {
+            $version = Start-Process -FilePath "C:\generic-worker\taskcluster-proxy.exe" -ArgumentList "--short-version" -Wait -NoNewWindow
+            $version | Should -be $proxy_version
+        }
+    }
+    Context "Livelog" {
+        It "Livelog exists" {
+            Test-Path "C:\generic-worker\livelog.exe" | Should -Be $true
+        }
+        It "Livelog version is correct" {
+            $version = Start-Process -FilePath "C:\generic-worker\livelog.exe" -ArgumentList "--short-version" -Wait -NoNewWindow
+            $version | Should -be $livelog_version
+        }
     }
 }
