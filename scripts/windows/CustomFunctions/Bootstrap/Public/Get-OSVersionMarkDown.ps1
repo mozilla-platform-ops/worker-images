158fb23ad8f531f512b345e9b416880f296e7e2a
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-07-16 00:40:50 -0400

    fix: :memo: Return version.ubr in markdown

--- scripts/windows/CustomFunctions/Bootstrap/Public/Get-OSVersionMarkDown.ps1
+++ scripts/windows/CustomFunctions/Bootstrap/Public/Get-OSVersionMarkDown.ps1
@@ -1,5 +1,7 @@
 function Get-OSVersionMarkDown {
-    $OSVersion = (Get-CimInstance -ClassName Win32_OperatingSystem).Version
-    $OSBuild = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion' UBR).UBR
-    return "$OSVersion Build $OSBuild"
+    $OSCurrentVersion = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion')
+    $result = "{0}.{1}" -f $OSCurrentVersion.CurrentBuildNumber, $OSCurrentVersion.UBR
+
+    return $result
+
 }
