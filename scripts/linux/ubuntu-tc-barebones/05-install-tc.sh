cafb9f40d451d38c5a94955f3db588f049546699
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-09-17 13:21:10 -0400

    Initial scripts for gcp image and alpha config

--- scripts/linux/ubuntu-tc-barebones/05-install-tc.sh
+++ scripts/linux/ubuntu-tc-barebones/05-install-tc.sh
@@ -3,10 +3,27 @@
 set -exv
 
 # init helpers
-helpers_dir=${MONOPACKER_HELPERS_DIR:-"/etc/monopacker/scripts"}
-for h in ${helpers_dir}/*.sh; do
-    . $h;
-done
+function retry {
+  set +e
+  local n=0
+  local max=5
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed" >&2
+        sleep_time=$((2 ** n))
+        echo "Sleeping $sleep_time seconds..." >&2
+        sleep $sleep_time
+        echo "Attempt $n/$max:" >&2
+      else
+        echo "Failed after $n attempts." >&2
+        exit 1
+      fi
+    }
+  done
+  set -e
+}
 
 #
 
