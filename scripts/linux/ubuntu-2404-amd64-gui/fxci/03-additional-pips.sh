6ab08f242dd8e4df0dd11fa5fdbc30755398a2b7
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-09-27 15:00:47 -0400

    Install zstandard but use break system packages

--- scripts/linux/ubuntu-2404-amd64-gui/fxci/03-additional-pips.sh
+++ scripts/linux/ubuntu-2404-amd64-gui/fxci/03-additional-pips.sh
@@ -2,4 +2,8 @@
 
 set -exv
 
-pip3 install zstandard
+## The warning error: externally-managed-environment popped up on 24.04 when trying to install zstandard using pip3
+## The --break-system-packages flag is used to ignore the warning error
+## Not sure if this will break other things
+## fetch-content needs zstandard
+pip3 install zstandard --break-system-packages
