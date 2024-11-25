9f111e9705cdc33bf45567eb65cffa436e8cb049
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-11-25 16:22:08 -0500

    Don't install uv (for now)

--- scripts/linux/ubuntu-2404-amd64-headless/fxci/02-additional-packages.sh
+++ scripts/linux/ubuntu-2404-amd64-headless/fxci/02-additional-packages.sh
@@ -50,5 +50,3 @@ MISC_PACKAGES+=(gcc make dkms pciutils linux-image-gcp linux-headers-gcp)
 
 # install the packages
 retry apt-get install -y ${MISC_PACKAGES[@]}
-# UV
-retry curl -LsSf https://astral.sh/uv/install.sh | sh
