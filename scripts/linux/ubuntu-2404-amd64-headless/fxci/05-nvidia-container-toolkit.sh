1f049071084f1840217d4c975b84531965eea088
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-11-25 16:32:54 -0500

    Add nvidia container toolkit instructions

---
+++ scripts/linux/ubuntu-2404-amd64-headless/fxci/05-nvidia-container-toolkit.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -exv
+
+function retry {
+  set +e
+  local n=0
+  local max=10
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed" >&2
+        sleep_time=$((2 ** n))
+        echo "Sleeping $sleep_time seconds..." >&2
+        sleep $sleep_time
+        echo "Attempt $n/$max:" >&2
+      else
+        echo "Failed after $n attempts." >&2
+        exit 1
+      fi
+    }
+  done
+  set -e
+}
+
+# Download the GPG key and save it in a keyring
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | \
+  gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+
+# Download the NVIDIA container toolkit list, modify it, and save it
+curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+  sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+  tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+apt-get update
+## Install nvidia-container-toolkit
+retry apt-get install -y nvidia-container-toolkit
+## Configure docker to use nvidia container runtime
+nvidia-ctk runtime configure --runtime=docker
+## Restart docker daemon to take effect
+systemctl restart docker
