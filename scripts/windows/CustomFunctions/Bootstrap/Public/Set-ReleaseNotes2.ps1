464a5d814ce9517bf70070385cdd6b3ae71b5ce2
Mark Cornmesser <mcornmesser@mozilla.com>
2025-04-10 01:51:55 -0700

    FEAT: troubleshooting change log generation

--- scripts/windows/CustomFunctions/Bootstrap/Public/Set-ReleaseNotes2.ps1
+++ scripts/windows/CustomFunctions/Bootstrap/Public/Set-ReleaseNotes2.ps1
@@ -55,206 +55,108 @@ function  Set-ReleaseNotes2 {
 	#$commitLog = git log "$LastDeployID..$DeploymentId" --pretty=format:"Commit: %H`nAuthor: %an`nDate: %ad`n`n%s`n%b`n---" --all --since="$sinceDate"
     #$commitLog = git log "$LastDeployID^..$DeploymentId" --pretty=format:"Commit: %H`nAuthor: %an`nDate: %ad`n`n%s`n%b`n---" --all --since="$sinceDate"
     $commitLog = git log "$LastDeployID...$DeploymentId" --ancestry-path --pretty=format:"Commit: %H`nAuthor: %an`nDate: %ad`n`n%s`n%b`n---" --all
-    write-host "$commitLog = git log "$LastDeployID...$DeploymentId" --ancestry-path --pretty=format:"Commit: %H`nAuthor: %an`nDate: %ad`n`n%s`n%b`n---" --all"
+    #write-host "$commitLog = git log "$LastDeployID...$DeploymentId" --ancestry-path --pretty=format:"Commit: %H`nAuthor: %an`nDate: %ad`n`n%s`n%b`n---" --all"
+
+    #$commitLog = (git log "$LastDeployID...$DeploymentId" --ancestry-path --pretty=format:"Commit: %H`nAuthor: %an`nDate: %ad`n`n%s`n%b`n---" --all) -join "`n"
 
     # Split commits by "Commit:"
     $commitEntries = $commitLog -split "(?=Commit: )"
+	Write-Host "Total commit entries found: $($commitEntries.Count)"
+$commitEntries | ForEach-Object { Write-Host "---- ENTRY START ----`n$_`n---- ENTRY END ----" }
 
     # Initialize an array to store commit objects
     $commitObjects = @()
     $currentCommit = $null
 
-    foreach ($entry in $commitEntries) {
-        # Trim whitespace and skip empty entries
-        $entry = $entry.Trim()
-        if ($entry -eq "") { continue }
-        # If the entry contains a commit hash, start a new structured object
-        if ($entry -match "^Commit: (?<Hash>\w{40})") {
-            # If there's an existing commit being processed, store it first
-            if ($null -ne $currentCommit -and ($Config -eq "" -or $currentCommit.Details.Roles -contains $Config)) {
-                $commitObjects += $currentCommit
-            }
+foreach ($entry in $commitEntries) {
+    $entry = $entry.Trim()
+    if ($entry -eq "") { continue }
 
-            # Construct the GitHub commit URL
-            $commitHash = $matches["Hash"]
-            $commitUrl = "$commitUrlBase$commitHash"
-
-            # Create a new commit object
-            $currentCommit = [PSCustomObject]@{
-                URL     = $commitUrl
-                Details = @{
-                    Jira    = "No Ticket"
-                    JiraURL = ""
-                    Bug     = ""
-                    BugURL  = ""
-                    Date    = ""
-                    Message = ""
-                    Roles   = @()
-                    Type    = "Uncategorized"
-                }
-            }
-            continue
+    if ($entry -match "^Commit: (?<Hash>\w{40})") {
+        # Save previous commit
+        if ($null -ne $currentCommit -and ($Config -eq "" -or $currentCommit.Details.Roles -contains $Config)) {
+            $commitObjects += $currentCommit
         }
 
-        # If the currentCommit is set, populate its fields
-        if ($null -ne $currentCommit) {
-            if ($entry -match "^Author: (?<Author>.+)") {
-                $author = $matches["Author"]
+        $commitHash = $matches["Hash"]
+        $commitUrl = "$commitUrlBase$commitHash"
+
+        # Start new commit object
+        $currentCommit = [PSCustomObject]@{
+            URL     = $commitUrl
+            Details = @{
+                Jira    = "No Ticket"
+                JiraURL = ""
+                Bug     = ""
+                BugURL  = ""
+                Date    = ""
+                Message = ""
+                Roles   = @()
+                Type    = "Uncategorized"
             }
-            elseif ($entry -match "^Date: (?<Date>.+)") {
-                # Extract date without time and append author
-                $dateParts = $matches["Date"] -split "\s+"
-                $formattedDate = "$($dateParts[0]) $($dateParts[1]) $($dateParts[2]) $($dateParts[4])"
-                if ($author) {
-                    $formattedDate += " by $author"
-                }
-                $currentCommit.Details.Date = $formattedDate
+        }
+        continue
+    }
+
+    # Parse current commit fields
+    if ($null -ne $currentCommit) {
+        if ($entry -match "^Author: (?<Author>.+)") {
+            $author = $matches["Author"]
+        }
+        elseif ($entry -match "^Date: (?<Date>.+)") {
+            $dateParts = $matches["Date"] -split "\s+"
+            $formattedDate = "$($dateParts[0]) $($dateParts[1]) $($dateParts[2]) $($dateParts[4])"
+            if ($author) {
+                $formattedDate += " by $author"
             }
-            # Extract Jira ticket and generate Jira URL separately
-            if ($entry -match "(?m)^(?<FullMessage>[A-Z]+: Jira:[A-Za-z0-9-]+ MSG: .+)$") {
-                $fullMessage = $matches["FullMessage"]
-                if ($fullMessage -match "^(?<Type>[A-Z]+): Jira:(?<Jira>[A-Za-z0-9-]+) MSG: (?<Message>.+)") {
-                    $currentCommit.Details.Type = $matches["Type"]
-                    $currentCommit.Details.Jira = $matches["Jira"]
-                    $currentCommit.Details.JiraURL = "$jiraUrlBase$($matches["Jira"])"
-                    $currentCommit.Details.Message = "$($currentCommit.Details.Type) - $($matches["Message"])"
-                }
+            $currentCommit.Details.Date = $formattedDate
+        }
+        else {
+		if ($entry -match "(?im)^\s*roles?:?\s*(?<Roles>[^\r\n]+)") {
+			# Strip off anything after "Location:" or similar extra fields
+			$rawRoles = $matches["Roles"] -split "Location:" | Select-Object -First 1
+			$currentCommit.Details.Roles = ($rawRoles -split ",") | ForEach-Object { $_.Trim() }
+			Write-Host "Parsed roles: $($currentCommit.Details.Roles -join ', ')"
+		}
+            if ($entry -match "^(?<Type>[A-Z]+):?\s") {
+                $currentCommit.Details.Type = $matches["Type"]
             }
-            # Extract bug number from message and generate Bugzilla URL
-            if ($currentCommit.Details.Message -match "\(Bug(?<Bug>\d+)\)") {
-                $bugNumber = $matches["Bug"]
-                $currentCommit.Details.Bug = $bugNumber
-                $currentCommit.Details.BugURL = "$bugUrlBase$bugNumber"
-                # Remove the Bug ID from the message itself
-                $currentCommit.Details.Message = $currentCommit.Details.Message -replace "\(Bug\d+\)", ""
+            if ($entry -match "(?i)Jira:?\s*(?<Jira>[A-Za-z0-9-]+)") {
+                $jira = $matches["Jira"]
+                $currentCommit.Details.Jira = $jira
+                $currentCommit.Details.JiraURL = "$jiraUrlBase$jira"
             }
-            elseif ($entry -match "(?m)^Roles: (?<Roles>.+)") {
-                $currentCommit.Details.Roles = ($matches["Roles"] -split ", ") | ForEach-Object { $_.Trim() }
+            if ($entry -match "(?i)Bug:?\s*(?<Bug>\d{5,})") {
+                $bug = $matches["Bug"]
+                $currentCommit.Details.Bug = $bug
+                $currentCommit.Details.BugURL = "$bugUrlBase$bug"
+            }
+            if ($entry -match "(?i)MSG:?\s*(?<Message>.+)$") {
+                $message = $matches["Message"].Trim()
+                $currentCommit.Details.Message = "$($currentCommit.Details.Type) - $message"
             }
         }
     }
+}
 
-    # Add the last processed commit object if it contains the role
-    if ($null -ne $currentCommit -and ($Config -eq "" -or $currentCommit.Details.Roles -contains $Config)) {
-        $commitObjects += $currentCommit
-    }
+# Don't forget the last commit
+if ($null -ne $currentCommit -and ($Config -eq "" -or $currentCommit.Details.Roles -contains $Config)) {
+    $commitObjects += $currentCommit
+}
 
-    ## The config will be the name of the configuration file (win11-64-2009) without the extension
-    ## We'll use this to generate release notes for each OS
-
-    Write-Log -message ('{0} :: Processing {1} {2} - {3:o}' -f $($MyInvocation.MyCommand.Name), $Config, $Version, (Get-Date).ToUniversalTime()) -severity 'DEBUG'
-
-    ## Let's install markdownPS just in case it isn't installed
-    Set-MarkdownPSModule
-
-    ## Let's get specific information about the OS
-    $OSBuild = Get-OSVersionMarkDown
-    if ($null -eq $OSBuild) {
-        $reason = "Unable to find OSBuild"
-        Write-Log -message ('{0} :: {1} - {2:o}' -f $($MyInvocation.MyCommand.Name), $reason, (Get-Date).ToUniversalTime()) -severity 'DEBUG'
-    }
-
-    ## Let's get all of the information about the OS
-    $OSVersionExtended = Get-OSVersionExtended 
-    if ($null -eq $OSVersionExtended) {
-        $reason = "Unable to find OSVersionExtended"
-        Write-Log -message ('{0} :: {1} - {2:o}' -f $($MyInvocation.MyCommand.Name), $reason, (Get-Date).ToUniversalTime()) -severity 'DEBUG'
-    }
-
-    ## Just return the OS version for manipulating the markdown header
-    $OSVersion = Get-OSVersion
-    if ($null -eq $OSVersion) {
-        $reason = "Unable to find OSVersion"
-        Write-Log -message ('{0} :: {1} - {2:o}' -f $($MyInvocation.MyCommand.Name), $reason, (Get-Date).ToUniversalTime()) -severity 'DEBUG'
-    }
-
-    ## Let's get the installed software installed on the OS
-    $InstalledSoftware = Get-InstalledSoftware | Where-Object {
-        ($psitem.DisplayName -match "\D") -and ($null -ne $psitem.DisplayVersion)
-    }
-    if ($null -eq $InstalledSoftware) {
-        $reason = "Unable to find Installed Software"
-        Write-Log -message ('{0} :: {1} - {2:o}' -f $($MyInvocation.MyCommand.Name), $reason, (Get-Date).ToUniversalTime()) -severity 'DEBUG'
-    }
-
-    ## Let's get speciifc information about the Mozilla Build environment
-    $mozillabuild = Get-WinFactsMozillaBuild
-    if ($null -eq $mozillabuild) {
-        $reason = "Unable to find facts for win mozilla build"
-        Write-Log -message ('{0} :: {1} - {2:o}' -f $($MyInvocation.MyCommand.Name), $reason, (Get-Date).ToUniversalTime()) -severity 'DEBUG'
-    }
-
-    ## Let's also get the python packages inside the Mozilla Build environment
-    $pythonPackages = Get-MozillaBuildPythonPackages -RequirementsFile "C:\requirements.txt"
-    if ($null -eq $pythonPackages) {
-        $reason = "Unable to find python packges in c:\requirements.txt"
-        Write-Log -message ('{0} :: {1} - {2:o}' -f $($MyInvocation.MyCommand.Name), $reason, (Get-Date).ToUniversalTime()) -severity 'DEBUG'
-    }
-
-    ## Now let's list out all software that isn't published by Microsoft
-    $InstalledSoftware_NotMicrosoft = $InstalledSoftware | Where-Object {
-        $PSItem.Publisher -notmatch "Microsoft"
-    } | ForEach-Object {
-        [PSCustomObject]@{
-            Name    = $PSItem.DisplayName
-            Version = $PSItem.DisplayVersion
-        }
-    } | Sort-Object -Property Name | Sort-Object Name, Version | Group-Object Name, Version | ForEach-Object { $_.Group[0] }
-
-    ## And now all software that is published by Microsoft
-    $InstalledSoftware_Microsoft = $InstalledSoftware | Where-Object {
-        $PSItem.Publisher -match "Microsoft"
-    } | ForEach-Object {
-        [PSCustomObject]@{
-            Name    = $PSItem.DisplayName
-            Version = $PSItem.DisplayVersion
-        }
-    } | Sort-Object -Property Name | Sort-Object Name, Version | Group-Object Name, Version | ForEach-Object { $_.Group[0] }
+ 
 
     ## Let's create the markdown file
     $markdown = ""
 
-    ## Start with the OS Information
-    switch -Wildcard ($OSVersion) {
-        "*win_10_*" {
-            $OSHeader = "Windows 10"
-        }
-        "*win_11_*" {
-            $OSHeader = "Windows 11"
-        }
-        "*win_2022_*" {
-            $OSHeader = "Windows 2022"
-        }
-        default {
-            $null
-        }
-    }
-	
-	$Header = $OSHeader + " Image Build " + $Version
 
-    if ($null -eq $OSVersion) {
-        $reason = "Unable to determine OSVersion"
-        Write-Log -message ('{0} :: {1} - {2:o}' -f $($MyInvocation.MyCommand.Name), $reason, (Get-Date).ToUniversalTime()) -severity 'DEBUG'
-    }
 
-    $markdown += New-MDHeader -Text $Header -Level 1
-    $markdown += "`n"
-    $lines = @(
-        "Config: $($Config)",
-        "OS Name: $($Header) $($OSVersionExtended.DisplayVersion)",
-        "OS Version: $($OSBuild)",
-        "Organization: $($Organization)",
-        "Repository: $($Repository)"
-        "Branch: $($Branch)",
-        "DeploymentId: $($DeploymentId)"
-    )
-    $markdown += New-MDHeader "Previous Version Notes" -Level 2
+
     foreach ($note in $Notes) {
         $markdown += "* $note`n"
     }
     $markdown += "`n"
-	$markdown += New-MDList -Lines $lines -Style Unordered
-    $markdown += New-MDHeader "Change Log" -Level 2
+
 
 	## Not using MD commands for correct variable interpolation
     foreach ($commit in $commitObjects) {
@@ -271,65 +173,6 @@ function  Set-ReleaseNotes2 {
         #$markdown += "`n---`n`n"
     }
 	
-	$markdown += New-MDHeader "Software Bill of Materials" -Level 2
-	
-    $markdown += New-MDHeader "Mozilla Build" -Level 2
-    $markdown += "`n"
-    $lines2 = @(
-        "Find more information about Mozilla Build on [Wiki](https://wiki.mozilla.org/MozillaBuild#Technical_Details)"
-    )
-    $markdown += New-MDAlert -Lines $lines2 -Style Important
-    
-    $lines3 = @(
-        "Mozilla Build: $($mozillabuild.custom_win_mozbld_version)"
-    )
-    
-    $markdown += New-MDList -Lines $lines3 -Style Unordered
-    
-    $markdown += New-MDHeader "Taskcluster Packages Installed" -Level 3
-    $markdown += "`n"
-    $markdown += Show-TaskclusterBinaries | New-MDTable
-    $markdown += "`n"
-
-    $markdown += New-MDHeader "Python Packages" -Level 3
-    $markdown += "`n"
-    $markdown += $pythonPackages | New-MDTable
-    $markdown += "`n"
-    
-    $markdown += New-MDHeader "Installed Software (Not Microsoft)" -Level 2
-    $markdown += "`n"
-    $markdown += $InstalledSoftware_NotMicrosoft | New-MDTable
-    $markdown += "`n"
-
-    $markdown += New-MDHeader "Installed Software (Microsoft)" -Level 2
-    $markdown += "`n"
-    $markdown += $InstalledSoftware_Microsoft | New-MDTable
-    
-    $markdown | Out-File "C:\software_report.md"
-
-    $markdown_content = Get-Content -Path "C:\software_report.md"
-    if ($null -eq $markdown_content) {
-        $reason = "Unable to find software_report.md"
-        Write-Log -message ('{0} :: {1} - {2:o}' -f $($MyInvocation.MyCommand.Name), $reason, (Get-Date).ToUniversalTime()) -severity 'DEBUG'
-    }
-
-    ## output the contents of the markdown file
-    Get-Content -Path "C:\software_report.md"
-
-    if (-Not (Test-Path "C:\software_report.md")) {
-        $reason = "Unable to find software_report.md after copy-item"
-        Write-Log -message ('{0} :: {1} - {2:o}' -f $($MyInvocation.MyCommand.Name), $reason, (Get-Date).ToUniversalTime()) -severity 'DEBUG'
-    }
-
-    if ($Version) {
-        Write-Log -message ('{0} :: Copying software_report.md to {1} - {2:o}' -f $($MyInvocation.MyCommand.Name), "(C:\$($Config)-$($Version).md)", (Get-Date).ToUniversalTime()) -severity 'DEBUG'
-        ## Now copy the software markdown file elsewhere to prep for uploading to azure
-        Copy-Item -Path "C:\software_report.md" -Destination "C:\$($Config)-$($Version).md"
-    }
-    else {
-        ## Now copy the software markdown file elsewhere to prep for uploading to azure
-        Copy-Item -Path "C:\software_report.md" -Destination "C:\$($Config).md"
-    }
-
-}
+	 $markdown | Out-File "C:\Users\markc\Desktop\2025\software_report.md"
+}  
 
