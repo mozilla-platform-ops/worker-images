60ceda1a8b0b5e22d5ae59a2311d4e8323b3ea82
Mark Cornmesser <mcornmesser@mozilla.com>
2025-06-11 13:44:03 -0700

    troubleshooting

--- bin/WorkerImages/Public/New-AzSharedWorkerImage.ps1
+++ bin/WorkerImages/Public/New-AzSharedWorkerImage.ps1
@@ -49,9 +49,37 @@ function New-AzSharedWorkerImage {
         return $merged
     }
 
+    function Log-FinalValue {
+        param (
+            [string] $Label,
+            [string] $Final,
+            [string] $Image,
+            [string] $Default
+        )
+        if ($Image -eq $Final) {
+            Write-Host "$Label = $Final (from image YAML)"
+        }
+        elseif ($Final -eq 'default') {
+            Write-Host "$Label = default ⚠️  (not overridden!)"
+        }
+        else {
+            Write-Host "$Label = $Final (overridden by default YAML)"
+        }
+    }
+
     $Y = Merge-YamlWithDefaults -ImageData $ImageYaml -DefaultData $DefaultYaml
 
-    # Set environment variables for Packer
+    # Debug logging
+    Log-FinalValue "puppet_version" $Y.vm["puppet_version"] $ImageYaml.vm["puppet_version"] $DefaultYaml.vm["puppet_version"]
+    Log-FinalValue "git_version"    $Y.vm["git_version"]    $ImageYaml.vm["git_version"]    $DefaultYaml.vm["git_version"]
+
+    Log-FinalValue "sourceBranch"        $Y.vm.tags["sourceBranch"]        $ImageYaml.vm.tags["sourceBranch"]        $DefaultYaml.vm.tags["sourceBranch"]
+    Log-FinalValue "sourceRepository"    $Y.vm.tags["sourceRepository"]    $ImageYaml.vm.tags["sourceRepository"]    $DefaultYaml.vm.tags["sourceRepository"]
+    Log-FinalValue "sourceOrganization"  $Y.vm.tags["sourceOrganization"]  $ImageYaml.vm.tags["sourceOrganization"]  $DefaultYaml.vm.tags["sourceOrganization"]
+    Log-FinalValue "deploymentId"        $Y.vm.tags["deploymentId"]        $ImageYaml.vm.tags["deploymentId"]        $DefaultYaml.vm.tags["deploymentId"]
+    Log-FinalValue "resource_group"      $Y.azure["managed_image_resource_group_name"] $ImageYaml.azure["managed_image_resource_group_name"] $DefaultYaml.azure["managed_image_resource_group_name"]
+
+    # Set environment variables
     $ENV:PKR_VAR_config = $Key
     $ENV:PKR_VAR_image_key_name = $Key
     $ENV:PKR_VAR_image_publisher = $Y.image["publisher"]
@@ -73,7 +101,6 @@ function New-AzSharedWorkerImage {
     $ENV:PKR_VAR_puppet_version = $Y.vm["puppet_version"]
     $ENV:PKR_VAR_git_version = $Y.vm["git_version"]
 
-    # Authentication and OIDC
     $ENV:PKR_VAR_client_id = $Client_ID
     $ENV:PKR_VAR_application_id = $Application_ID
     $ENV:PKR_VAR_tenant_id = $Tenant_ID
@@ -81,40 +108,32 @@ function New-AzSharedWorkerImage {
     $ENV:PKR_VAR_oidc_request_url = $oidc_request_url
     $ENV:PKR_VAR_oidc_request_token = $oidc_request_token
 
-    # Temp resource group
     $ENV:PKR_VAR_temp_resource_group_name = ('{0}-{1}-{2}-pkrtmp' -f `
         $ENV:PKR_VAR_worker_pool_id, `
         $ENV:PKR_VAR_deployment_id, `
         (Get-Random -Maximum 999))
 
-    # Managed image name based on key
     switch -Wildcard ($Key) {
         "*alpha2*" {
             $PackerForceBuild = $true
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-alpha2' -f `
-                $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku)
+            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-alpha2' -f $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku)
         }
         "*alpha*" {
             $PackerForceBuild = $true
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-alpha' -f `
-                $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku)
+            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-alpha' -f $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku)
         }
         "*beta*" {
             $PackerForceBuild = $true
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-beta' -f `
-                $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku)
+            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-beta' -f $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku)
         }
         "*next*" {
             $PackerForceBuild = $true
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-next' -f `
-                $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku)
+            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-next' -f $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku)
         }
         Default {
             $PackerForceBuild = $false
             $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-{2}' -f `
-                $ENV:PKR_VAR_worker_pool_id, `
-                $ENV:PKR_VAR_image_sku, `
-                $ENV:PKR_VAR_deployment_id)
+                $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku, $ENV:PKR_VAR_deployment_id)
         }
     }
 
