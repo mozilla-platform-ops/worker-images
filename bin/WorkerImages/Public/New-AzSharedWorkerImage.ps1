9f0f01bc256cd8ea5fabcec2f52fd856bcd39b2d
markcor <mcornmesser@mozilla.com>
2025-06-17 07:36:45 -0700

    Relops 1263: FEAT:  Add default Windows data file and support (#328)
    * RELOPS-1263 add default yaml
    
    * update 10-64-2009 yaml file
    
    * troubleshooting
    
    * Test building win11-24h2 with default data
    
    * Update ronin hash for testing
    
    * testing
    
    * quote default in config file
    
    * Update New-AZ shared image script to pick up defualts for git and puppet.
    
    * Update New-AZ shared image script to pick up defualts for git and puppet.
    
    * Update New-AZ shared image script to pick up defualts for git and puppet.
    
    * troubleshooting
    
    * update AZ pre req install script
    
    * revert last change
    
    * testing azprereq script
    
    * revert changes and add more debugging
    
    * remove debug  message
    
    * revert prereq script
    
    * testing prerequ script with fallback to default values
    
    * testing default overwrite
    
    * Update config files
    
    * corrections
    
    * testing
    
    * testing
    
    * corrections
    
    * revert version

--- bin/WorkerImages/Public/New-AzSharedWorkerImage.ps1
+++ bin/WorkerImages/Public/New-AzSharedWorkerImage.ps1
@@ -1,87 +1,147 @@
 function New-AzSharedWorkerImage {
     [CmdletBinding()]
     param (
-        [String]
-        $Key,
-
-        [String]
-        $Client_ID,
-
-        [String]
-        $Client_Secret,
-
-        [String]
-        $Application_ID,
-
-        [String]
-        $oidc_request_url,
-
-        [String]
-        $oidc_request_token,
-
-        [String]
-        $Subscription_ID,
-
-        [String]
-        $Tenant_ID
+        [String] $Key,
+        [String] $Client_ID,
+        [String] $Client_Secret,
+        [String] $Application_ID,
+        [String] $oidc_request_url,
+        [String] $oidc_request_token,
+        [String] $Subscription_ID,
+        [String] $Tenant_ID
     )
 
     Set-PSRepository PSGallery -InstallationPolicy Trusted
     Install-Module powershell-yaml -ErrorAction Stop
-    $YAML = Convertfrom-Yaml (Get-Content "config/$key.yaml" -raw)
-    $ENV:PKR_VAR_config = $key
-    $ENV:PKR_VAR_image_key_name = $key
-    $ENV:PKR_VAR_image_publisher = $YAML.image["publisher"]
-    $ENV:PKR_VAR_resource_group = $yaml.azure["managed_image_resource_group_name"]
-    $ENV:PKR_VAR_image_offer = $YAML.image["offer"]
-    $ENV:PKR_VAR_image_sku = $YAML.image["sku"]
-    $ENV:PKR_VAR_image_version = $YAML.image["version"]
-    $ENV:PKR_VAR_vm_size = $YAML.vm["size"]
-    $ENV:PKR_VAR_base_image = $YAML.vm.tags["base_image"]
-    $ENV:PKR_VAR_source_branch = $YAML.vm.tags["sourceBranch"]
-    $ENV:PKR_VAR_source_repository = $YAML.vm.tags["sourceRepository"]
-    $ENV:PKR_VAR_source_organization = $YAML.vm.tags["sourceOrganization"]
-    $ENV:PKR_VAR_deployment_id = $YAML.vm.tags["deploymentId"]
-    $ENV:PKR_VAR_bootstrap_script = $YAML.azure["bootstrapscript"]
-    $ENV:PKR_VAR_gallery_name = $YAML.sharedimage["gallery_name"]
-    $ENV:PKR_VAR_image_name = $YAML.sharedimage["image_name"]
-    $ENV:PKR_VAR_sharedimage_version = $YAML.sharedimage["image_version"]
+
+    $DefaultYaml = ConvertFrom-Yaml (Get-Content "config/windows_production_defaults.yaml" -Raw)
+    $ImageYaml   = ConvertFrom-Yaml (Get-Content "config/$Key.yaml" -Raw)
+
+    function Merge-YamlWithDefaults {
+        param (
+            [hashtable] $ImageData,
+            [hashtable] $DefaultData
+        )
+        $merged = @{}
+        $allKeys = $ImageData.Keys + $DefaultData.Keys | Select-Object -Unique
+        foreach ($key in $allKeys) {
+            $imageVal = $ImageData[$key]
+            $defaultVal = $DefaultData[$key]
+
+            if ($imageVal -is [hashtable] -and $defaultVal -is [hashtable]) {
+                $merged[$key] = Merge-YamlWithDefaults -ImageData $imageVal -DefaultData $defaultVal
+            }
+            elseif ($imageVal -is [System.Collections.IEnumerable] -and
+                    -not ($imageVal -is [string]) -and
+                    $imageVal.Count -gt 0) {
+                $merged[$key] = $imageVal
+            }
+            elseif ($imageVal -eq 'default' -and $null -ne $defaultVal -and $defaultVal -ne 'default') {
+                $merged[$key] = $defaultVal
+            }
+            elseif ($null -ne $imageVal -and $imageVal -ne '' -and $imageVal -ne 'default') {
+                $merged[$key] = $imageVal
+            }
+            elseif ($null -ne $defaultVal -and $defaultVal -ne 'default') {
+                $merged[$key] = $defaultVal
+            }
+        }
+        return $merged
+    }
+
+    function Log-FinalValue {
+        param (
+            [string] $Label,
+            [string] $Final,
+            [string] $Image,
+            [string] $Default
+        )
+        if ($Image -eq $Final) {
+            Write-Host "$Label = $Final (from image YAML)"
+        }
+        elseif ($Final -eq 'default') {
+            Write-Host "$Label = default ⚠️  (not overridden!)"
+        }
+        else {
+            Write-Host "$Label = $Final (overridden by default YAML)"
+        }
+    }
+
+    $Y = Merge-YamlWithDefaults -ImageData $ImageYaml -DefaultData $DefaultYaml
+
+    # Debug logging
+    Log-FinalValue "puppet_version" $Y.vm["puppet_version"] $ImageYaml.vm["puppet_version"] $DefaultYaml.vm["puppet_version"]
+    Log-FinalValue "git_version"    $Y.vm["git_version"]    $ImageYaml.vm["git_version"]    $DefaultYaml.vm["git_version"]
+
+    Log-FinalValue "sourceBranch"        $Y.vm.tags["sourceBranch"]        $ImageYaml.vm.tags["sourceBranch"]        $DefaultYaml.vm.tags["sourceBranch"]
+    Log-FinalValue "sourceRepository"    $Y.vm.tags["sourceRepository"]    $ImageYaml.vm.tags["sourceRepository"]    $DefaultYaml.vm.tags["sourceRepository"]
+    Log-FinalValue "sourceOrganization"  $Y.vm.tags["sourceOrganization"]  $ImageYaml.vm.tags["sourceOrganization"]  $DefaultYaml.vm.tags["sourceOrganization"]
+    Log-FinalValue "deploymentId"        $Y.vm.tags["deploymentId"]        $ImageYaml.vm.tags["deploymentId"]        $DefaultYaml.vm.tags["deploymentId"]
+    Log-FinalValue "resource_group"      $Y.azure["managed_image_resource_group_name"] $ImageYaml.azure["managed_image_resource_group_name"] $DefaultYaml.azure["managed_image_resource_group_name"]
+
+    # Set environment variables
+    $ENV:PKR_VAR_config = $Key
+    $ENV:PKR_VAR_image_key_name = $Key
+    $ENV:PKR_VAR_image_publisher = $Y.image["publisher"]
+    $ENV:PKR_VAR_image_offer = $Y.image["offer"]
+    $ENV:PKR_VAR_image_sku = $Y.image["sku"]
+    $ENV:PKR_VAR_image_version = $Y.image["version"]
+    $ENV:PKR_VAR_resource_group = $Y.azure["managed_image_resource_group_name"]
+    $ENV:PKR_VAR_vm_size = $Y.vm["size"]
+    $ENV:PKR_VAR_base_image = $Y.vm.tags["base_image"]
+    $ENV:PKR_VAR_source_branch = $Y.vm.tags["sourceBranch"]
+    $ENV:PKR_VAR_source_repository = $Y.vm.tags["sourceRepository"]
+    $ENV:PKR_VAR_source_organization = $Y.vm.tags["sourceOrganization"]
+    $ENV:PKR_VAR_deployment_id = $Y.vm.tags["deploymentId"]
+    $ENV:PKR_VAR_worker_pool_id = $Y.vm.tags["worker_pool_id"]
+    $ENV:PKR_VAR_bootstrap_script = $Y.azure["bootstrapscript"]
+    $ENV:PKR_VAR_gallery_name = $Y.sharedimage["gallery_name"]
+    $ENV:PKR_VAR_image_name = $Y.sharedimage["image_name"]
+    $ENV:PKR_VAR_sharedimage_version = $Y.sharedimage["image_version"]
+    $ENV:PKR_VAR_puppet_version = $Y.vm["puppet_version"]
+    $ENV:PKR_VAR_git_version = $Y.vm["git_version"]
+
     $ENV:PKR_VAR_client_id = $Client_ID
-    $ENV:PKR_VAR_temp_resource_group_name = ('{0}-{1}-{2}-pkrtmp' -f $YAML.vm.tags["worker_pool_id"], $YAML.vm.tags["deploymentId"], (Get-Random -Maximum 999))
+    $ENV:PKR_VAR_application_id = $Application_ID
     $ENV:PKR_VAR_tenant_id = $Tenant_ID
     $ENV:PKR_VAR_subscription_id = $Subscription_ID
-    $ENV:PKR_VAR_application_id = $Application_ID
     $ENV:PKR_VAR_oidc_request_url = $oidc_request_url
     $ENV:PKR_VAR_oidc_request_token = $oidc_request_token
-    $ENV:PKR_VAR_worker_pool_id = $YAML.vm.tags["worker_pool_id"]
-    switch -Wildcard ($key) {
+
+    $ENV:PKR_VAR_temp_resource_group_name = ('{0}-{1}-{2}-pkrtmp' -f `
+        $ENV:PKR_VAR_worker_pool_id, `
+        $ENV:PKR_VAR_deployment_id, `
+        (Get-Random -Maximum 999))
+
+    switch -Wildcard ($Key) {
         "*alpha2*" {
             $PackerForceBuild = $true
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-alpha2' -f $YAML.vm.tags["worker_pool_id"], $ENV:PKR_VAR_image_sku)
+            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-alpha2' -f $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku)
         }
         "*alpha*" {
             $PackerForceBuild = $true
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-alpha' -f $YAML.vm.tags["worker_pool_id"], $ENV:PKR_VAR_image_sku)
+            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-alpha' -f $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku)
         }
         "*beta*" {
             $PackerForceBuild = $true
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-beta' -f $YAML.vm.tags["worker_pool_id"], $ENV:PKR_VAR_image_sku)
+            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-beta' -f $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku)
         }
         "*next*" {
             $PackerForceBuild = $true
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-next' -f $YAML.vm.tags["worker_pool_id"],  $ENV:PKR_VAR_image_sku)
-        } 
+            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-next' -f $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku)
+        }
         Default {
             $PackerForceBuild = $false
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-{2}' -f $YAML.vm.tags["worker_pool_id"], $ENV:PKR_VAR_image_sku, $YAML.vm.tags["deploymentId"])
+            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-{2}' -f `
+                $ENV:PKR_VAR_worker_pool_id, $ENV:PKR_VAR_image_sku, $ENV:PKR_VAR_deployment_id)
         }
     }
+
     Write-Host "Building $($ENV:PKR_VAR_managed_image_name) in $($ENV:PKR_VAR_temp_resource_group_name)"
     packer init azure.pkr.hcl
     if ($PackerForceBuild) {
         packer build --only azure-arm.sig -force azure.pkr.hcl
-    }
-    else {
+    } else {
         packer build --only azure-arm.sig azure.pkr.hcl
     }
 }
