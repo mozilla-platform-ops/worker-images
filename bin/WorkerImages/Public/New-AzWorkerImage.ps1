3ddd4dffd67b56a4018201780704dac0d1b3886d
Mark Cornmesser <mcornmesser@mozilla.com>
2025-07-17 14:36:14 -0700

    initial TC image support

--- bin/WorkerImages/Public/New-AzWorkerImage.ps1
+++ bin/WorkerImages/Public/New-AzWorkerImage.ps1
@@ -1,83 +1,135 @@
 function New-AzWorkerImage {
     [CmdletBinding()]
     param (
-        [String]
-        $Key,
+        [Parameter(Mandatory = $true)]
+        [String] $Key,
 
-        [String]
-        $Location,
+        [Parameter(Mandatory = $true)]
+        [String] $Location,
 
-        [String]
-        $Client_ID,
+        [Parameter(Mandatory = $true)]
+        [String] $Client_ID,
 
-        [String]
-        $Subscription_ID,
+        [Parameter(Mandatory = $true)]
+        [String] $Subscription_ID,
 
-        [String]
-        $Tenant_ID,
+        [Parameter(Mandatory = $true)]
+        [String] $Tenant_ID,
 
-        [String]
-        $Application_ID,
+        [Parameter(Mandatory = $true)]
+        [String] $Application_ID,
 
-        [String]
-        $oidc_request_url,
+        [Parameter(Mandatory = $true)]
+        [String] $oidc_request_url,
 
-        [String]
-        $oidc_request_token,
+        [Parameter(Mandatory = $true)]
+        [String] $oidc_request_token,
 
-        [Switch]
-        $PackerDebug
+        [Parameter(Mandatory = $false)]
+        [String] $Team,
+
+        [Switch] $PackerDebug
     )
 
     Set-PSRepository PSGallery -InstallationPolicy Trusted
     Install-Module powershell-yaml -ErrorAction Stop
-    $YAML = Convertfrom-Yaml (Get-Content "config/$key.yaml" -raw)
-    $ENV:PKR_VAR_config = $key
+
+    switch ($Team) {
+        "tceng" {
+            $YamlPath = "config/tceng/$Key.yaml"
+            $PackerHCLPath = "packer\tceng.azure.pkr.hcl"
+            $ENV:PKR_VAR_team_key = $Team
+
+            $uuidBytes = [System.Text.Encoding]::UTF8.GetString(
+                [System.Convert]::FromBase64String(
+                    [System.Convert]::ToBase64String(
+                        (1..256 | ForEach-Object { Get-Random -Minimum 97 -Maximum 122 } | ForEach-Object { [byte]$_ })
+                    )
+                )
+            )
+            $uuid = ($uuidBytes -replace '[^a-z0-9]', '')[0..19] -join ''
+            $ENV:PKR_VAR_uuid = $uuid
+        }
+        default {
+            $YamlPath = "config/$Key.yaml"
+            $PackerHCLPath = "azure.pkr.hcl"
+            if ($Team) {
+                $ENV:PKR_VAR_team_key = $Team
+            }
+        }
+    }
+
+    if (-not (Test-Path $YamlPath)) {
+        throw "YAML file not found at: $YamlPath"
+    }
+
+    $YAML = ConvertFrom-Yaml (Get-Content $YamlPath -Raw)
+
+    $ENV:PKR_VAR_config = $Key
     $ENV:PKR_VAR_location = $Location
-    $ENV:PKR_VAR_image_key_name = $key
-    $ENV:PKR_VAR_image_publisher = $YAML.image["publisher"]
-    $ENV:PKR_VAR_resource_group = $yaml.azure["managed_image_resource_group_name"]
-    $ENV:PKR_VAR_image_offer = $YAML.image["offer"]
-    $ENV:PKR_VAR_image_sku = $YAML.image["sku"]
-    $ENV:PKR_VAR_image_version = $YAML.image["version"]
-    $ENV:PKR_VAR_vm_size = $YAML.vm["size"]
-    $ENV:PKR_VAR_base_image = $YAML.vm.tags["base_image"]
-    $ENV:PKR_VAR_source_branch = $YAML.vm.tags["sourceBranch"]
-    $ENV:PKR_VAR_source_repository = $YAML.vm.tags["sourceRepository"]
-    $ENV:PKR_VAR_source_organization = $YAML.vm.tags["sourceOrganization"]
-    $ENV:PKR_VAR_deployment_id = $YAML.vm.tags["deploymentId"]
-    $ENV:PKR_VAR_bootstrap_script = $YAML.azure["bootstrapscript"]
-    $ENV:PKR_VAR_client_id = $Client_ID
-    $ENV:PKR_VAR_temp_resource_group_name = ('{0}-{1}-{2}-pkrtmp' -f $YAML.vm.tags["worker_pool_id"], $YAML.vm.tags["deploymentId"], (Get-Random -Maximum 999))
-    $ENV:PKR_VAR_tenant_id = $Tenant_ID
-    $ENV:PKR_VAR_subscription_id = $Subscription_ID
-    $ENV:PKR_VAR_application_id = $Application_ID
-    $ENV:PKR_VAR_oidc_request_url = $oidc_request_url
+    $ENV:PKR_VAR_image_key_name = $Key
+
+    if ($YAML.image["publisher"]) { $ENV:PKR_VAR_image_publisher = $YAML.image["publisher"] }
+    if ($YAML.image["offer"])     { $ENV:PKR_VAR_image_offer     = $YAML.image["offer"] }
+    if ($YAML.image["sku"])       { $ENV:PKR_VAR_image_sku       = $YAML.image["sku"] }
+    if ($YAML.image["version"])   { $ENV:PKR_VAR_image_version   = $YAML.image["version"] }
+
+    if ($YAML.azure["managed_image_resource_group_name"]) {
+        $ENV:PKR_VAR_resource_group = $YAML.azure["managed_image_resource_group_name"]
+    }
+    if ($YAML.azure["managed_image_storage_account_type"]) {
+        $ENV:PKR_VAR_managed_image_storage_account_type = $YAML.azure["managed_image_storage_account_type"]
+    }
+    if ($YAML.azure["bootstrapscript"]) {
+        $ENV:PKR_VAR_bootstrap_script = $YAML.azure["bootstrapscript"]
+    }
+
+    if ($YAML.vm["vm_size"])              { $ENV:PKR_VAR_vm_size              = $YAML.vm["vm_size"] }
+    if ($YAML.vm["taskcluster_version"])  { $ENV:PKR_VAR_taskcluster_version  = $YAML.vm["taskcluster_version"] }
+    if ($YAML.vm["taskcluster_ref"])      { $ENV:PKR_VAR_taskcluster_ref      = $YAML.vm["taskcluster_ref"] }
+    if ($YAML.vm["taskcuster_repo"])      { $ENV:PKR_VAR_taskcuster_repo      = $YAML.vm["taskcuster_repo"] }
+    if ($YAML.vm["providerType"])         { $ENV:PKR_VAR_provider_type        = $YAML.vm["providerType"] }
+
+    if ($YAML.vm.tags["base_image"])        { $ENV:PKR_VAR_base_image        = $YAML.vm.tags["base_image"] }
+    if ($YAML.vm.tags["sourceBranch"])      { $ENV:PKR_VAR_source_branch     = $YAML.vm.tags["sourceBranch"] }
+    if ($YAML.vm.tags["sourceRepository"])  { $ENV:PKR_VAR_source_repository = $YAML.vm.tags["sourceRepository"] }
+    if ($YAML.vm.tags["sourceOrganization"]){ $ENV:PKR_VAR_source_organization = $YAML.vm.tags["sourceOrganization"] }
+    if ($YAML.vm.tags["deploymentId"])      { $ENV:PKR_VAR_deployment_id     = $YAML.vm.tags["deploymentId"] }
+    if ($YAML.vm.tags["worker_pool_id"])    { $ENV:PKR_VAR_worker_pool_id    = $YAML.vm.tags["worker_pool_id"] }
+
+    if ($YAML.vm.tags["worker_pool_id"] -and $YAML.vm.tags["deploymentId"]) {
+        $ENV:PKR_VAR_temp_resource_group_name = ('{0}-{1}-{2}-pkrtmp' -f $YAML.vm.tags["worker_pool_id"], $YAML.vm.tags["deploymentId"], (Get-Random -Maximum 999))
+    }
+
+    $ENV:PKR_VAR_client_id          = $Client_ID
+    $ENV:PKR_VAR_tenant_id          = $Tenant_ID
+    $ENV:PKR_VAR_subscription_id    = $Subscription_ID
+    $ENV:PKR_VAR_application_id     = $Application_ID
+    $ENV:PKR_VAR_oidc_request_url   = $oidc_request_url
     $ENV:PKR_VAR_oidc_request_token = $oidc_request_token
-    $ENV:PKR_VAR_worker_pool_id = $YAML.vm.tags["worker_pool_id"]
-    switch -Wildcard ($key) {
-        "*alpha2*" {
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-{2}-alpha2' -f $YAML.vm.tags["worker_pool_id"], $Location, $ENV:PKR_VAR_image_sku)
-        }
-        "*alpha*" {
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-{2}-alpha' -f $YAML.vm.tags["worker_pool_id"], $Location, $ENV:PKR_VAR_image_sku)
-        }
-        "*beta*" {
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-{2}-beta' -f $YAML.vm.tags["worker_pool_id"], $Location, $ENV:PKR_VAR_image_sku)
-        }
-        "*next*" {
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-{2}-next' -f $YAML.vm.tags["worker_pool_id"], $Location, $ENV:PKR_VAR_image_sku)
-        } 
-        Default {
-            $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-{2}-{3}' -f $YAML.vm.tags["worker_pool_id"], $Location, $ENV:PKR_VAR_image_sku, $YAML.vm.tags["deploymentId"])
+
+    if ($Team -eq "tceng" -and $ENV:PKR_VAR_uuid) {
+        $ENV:PKR_VAR_managed_image_name = "markco-test-imageset-$($ENV:PKR_VAR_uuid)-$Location"
+    } else {
+        switch -Wildcard ($Key) {
+            "*alpha2*" {
+                $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-{2}-alpha2' -f $YAML.vm.tags["worker_pool_id"], $Location, $ENV:PKR_VAR_image_sku)
+            }
+            "*alpha*" {
+                $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-{2}-alpha' -f $YAML.vm.tags["worker_pool_id"], $Location, $ENV:PKR_VAR_image_sku)
+            }
+            Default {
+                $ENV:PKR_VAR_managed_image_name = ('{0}-{1}-{2}-{3}' -f $YAML.vm.tags["worker_pool_id"], $Location, $ENV:PKR_VAR_image_sku, $YAML.vm.tags["deploymentId"])
+            }
         }
     }
+
     Write-Host "Building $($ENV:PKR_VAR_managed_image_name) in $($ENV:PKR_VAR_temp_resource_group_name)"
-    packer init azure.pkr.hcl
+
+    packer init $PackerHCLPath
     if ($PackerDebug) {
-        packer build -debug --only azure-arm.nonsig -force azure.pkr.hcl
+        packer build -debug --only azure-arm.nonsig -force $PackerHCLPath
+    } else {
+        packer build --only azure-arm.nonsig -force $PackerHCLPath
     }
-    else {
-        packer build --only azure-arm.nonsig -force azure.pkr.hcl
-    } 
 }
---
+++ packer/tceng-azure.pkr.hcl
@@ -0,0 +1,111 @@
+// Inline Powershell Commands need a ";" inside quotations. Except for last command.
+// Without it Packer will treat all commands as single line.
+
+packer {
+  required_plugins {
+    azure = {
+      version = ">= 1.4.5"
+      source  = "github.com/hashicorp/azure"
+    }
+  }
+}
+
+locals {
+  sbom_name = var.config
+}
+
+// Image configuration driven by environment variables set by wrapper script (e.g., parsed from YAML)
+variable "image_publisher"       { default = env("image_publisher") }
+variable "image_offer"           { default = env("image_offer") }
+variable "image_sku"             { default = env("image_sku") }
+variable "image_version"         { default = env("image_version") }
+variable "vm_size"               { default = env("vm_size") }
+variable "location"              { default = env("location") }
+
+variable "managed_image_name"    { default = env("managed_image_name") }
+variable "resource_group"        { default = env("resource_group") }
+variable "gallery_name"          { default = env("gallery_name") }
+variable "sharedimage_version"   { default = env("sharedimage_version") }
+variable "bootstrap_script"      { default = env("bootstrap_script") }
+
+// Taskcluster metadata
+variable "taskcluster_ref"       { default = env("taskcluster_ref") }
+variable "taskcluster_repo"      { default = env("taskcluster_repo") }
+variable "provider_type"         { default = env("provider_type") }
+
+// Authentication & metadata
+variable "client_id"              { default = env("client_id") }
+variable "tenant_id"              { default = env("tenant_id") }
+variable "subscription_id"        { default = env("subscription_id") }
+variable "oidc_request_url"       { default = env("ACTIONS_ID_TOKEN_REQUEST_URL") }
+variable "oidc_request_token"     { default = env("ACTIONS_ID_TOKEN_REQUEST_TOKEN") }
+
+source "azure-arm" "sig" {
+  communicator                = "winrm"
+  winrm_insecure             = true
+  winrm_timeout              = "3m"
+  winrm_use_ssl              = true
+  winrm_username             = "packer"
+
+  oidc_request_url           = var.oidc_request_url
+  oidc_request_token         = var.oidc_request_token
+  client_id                  = var.client_id
+  subscription_id            = var.subscription_id
+  tenant_id                  = var.tenant_id
+
+  os_type                    = "Windows"
+  image_publisher            = var.image_publisher
+  image_offer                = var.image_offer
+  image_sku                  = var.image_sku
+  image_version              = var.image_version
+
+  location                   = var.location
+  vm_size                    = var.vm_size
+  temp_resource_group_name  = "packer-temp-${timestamp()}"
+  async_resourcegroup_delete = true
+
+  shared_image_gallery_destination {
+    subscription    = var.subscription_id
+    resource_group  = var.resource_group
+    gallery_name    = var.gallery_name
+    image_name      = var.managed_image_name
+    image_version   = var.sharedimage_version
+    replication_regions = [
+      "centralus",
+      "eastus",
+      "northcentralus",
+      "southcentralus",
+      "westus",
+      "westus2"
+    ]
+  }
+
+  azure_tags = {
+    base_image     = "${var.image_publisher}:${var.image_offer}:${var.image_sku}:${var.image_version}"
+    managed_by     = "packer"
+    deployment_id  = var.sharedimage_version
+  }
+}
+
+build {
+  sources = ["source.azure-arm.sig"]
+
+  provisioner "file" {
+    source      = "scripts/windows/tceng/${var.bootstrap_script}.ps1"
+    destination = "C:/Windows/Temp/bootstrap.ps1"
+  }
+
+  provisioner "powershell" {
+    inline = [
+      "& 'C:/Windows/Temp/bootstrap.ps1' -providerType '${var.provider_type}' -TASKCLUSTER_REF '${var.taskcluster_ref}' -TASKCLUSTER_REPO '${var.taskcluster_repo}'"
+    ]
+  }
+
+  provisioner "windows-restart" {}
+
+  provisioner "powershell" {
+    inline = [
+      "& $env:SystemRoot\\System32\\Sysprep\\Sysprep.exe /generalize /oobe /shutdown /quiet"
+    ]
+  }
+}
