874cfce78e2411729e294695cf8f9bdb178d2601
Jonathan Moss <jwmoss88@gmail.com>
2025-07-23 18:12:19 -0400

    fix puppet windows service test

--- tests/win/disable_puppet_service.tests.ps1
+++ tests/win/disable_puppet_service.tests.ps1
@@ -1,10 +1,10 @@
 Describe "Puppet Windows Service" {
     Context "puppet service" {
         It "Puppet service exists" {
-            Get-Service $_ | Should -Not -Be $null
+            Get-Service "puppet" | Should -Not -Be $null
         }
         It "Puppet service exists is disabled" {
-            (Get-Service $_).Status | Should -Be "Stopped"
+            (Get-Service "puppet").Status | Should -Be "Stopped"
         }
     }
 }
