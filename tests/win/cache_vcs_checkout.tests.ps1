468e0d66403cacb8ce6ac6fd62658a2071c7397d
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2025-08-11 19:23:33 -0400

    fix tests to check if .hg dir exists

--- tests/win/cache_vcs_checkout.tests.ps1
+++ tests/win/cache_vcs_checkout.tests.ps1
@@ -3,7 +3,7 @@ Describe "Cache VCS Checkout" {
         Test-Path "C:\mozilla-unified" | Should -Be True
     }
     It "Mozilla Unified Contents" {
-        Test-Path "C:\mozilla-unified\taskcluster" | Should -Be True
+        Test-Path "C:\mozilla-unified\.hg" | Should -Be True
     }
     It "Mozilla Unified Cache Folder Environment Variable" {
         $ENV:VCS_CHECKOUT | Should -Be "C:\mozilla-unified"
