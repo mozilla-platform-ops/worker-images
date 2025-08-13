fb87d6287fc643b0d6e4628971531370ddf303e2
Jonathan Moss <jwmoss88@gmail.com>
2025-08-13 14:55:48 -0400

    check for hg-shared on os drive

--- tests/win/cache_vcs_checkout.tests.ps1
+++ tests/win/cache_vcs_checkout.tests.ps1
@@ -1,11 +1,8 @@
 Describe "Cache VCS Checkout" {
     It "Mozilla Unified Directory" {
-        Test-Path "C:\mozilla-unified" | Should -Be True
+        Test-Path "C:\hg-shared" | Should -Be True
     }
     It "Mozilla Unified Contents" {
-        Test-Path "C:\mozilla-unified\.hg" | Should -Be True
-    }
-    It "Mozilla Unified Cache Folder Environment Variable" {
-        $ENV:VCS_CHECKOUT | Should -Be "C:\mozilla-unified"
+        Test-Path "C:\hg-shared\.hg" | Should -Be True
     }
 }
