9c3fde5f42ac1db3b2cbff616abeb4f895ad1016
Jonathan Moss <jwmoss88@gmail.com>
2025-08-13 14:56:02 -0400

    run robustcheckout to pre-populate mozilla unified cache

--- scripts/windows/CustomFunctions/Bootstrap/Public/Get-MozillaUnified.ps1
+++ scripts/windows/CustomFunctions/Bootstrap/Public/Get-MozillaUnified.ps1
@@ -2,7 +2,10 @@ function Get-MozillaUnified {
     [CmdletBinding()]
     param (
         [String]
-        $ClonePath = "C:\mozilla-unified",
+        $ClonePath = "C:\hg-shared",
+
+        [String]
+        $TempClonePath = "C:\hg-shared_temp"
         
         [String]
         $Repository = "https://hg.mozilla.org/mozilla-unified",
@@ -15,26 +18,46 @@ function Get-MozillaUnified {
     
     try {
         Write-Log -message "Cloning $Repository to $ClonePath" -severity 'INFO'
+
+        $null = New-Item -Path $TempClonePath -ItemType Directory -Force
+        if (-Not (Test-Path $TempClonePath)) {
+            Write-Log -message "Failed to create temporary clone path: $TempClonePath" -severity 'ERROR'
+            exit 1
+        }
+
         $Splat = @{
             FilePath = $Hg
-            ArgumentList = @("clone", $Repository, $ClonePath)
+            ArgumentList = @(
+                "robustcheckout",
+                "--sharebase",
+                $ClonePath,
+                "--config",
+                "extensions.robustcheckout=C:\\mozilla-build\\robustcheckout.py",
+                "--revision",
+                "tip",
+                $Repository,
+                $TempClonePath
+            )
             Wait = $true
             PassThru = $true
             NoNewWindow = $true
         }
         $hgProcess = Start-Process @Splat
         
+        ## Now remove the tempClonePath
+        Remove-Item -Path $TempClonePath -Recurse -Force
+
         if ($hgProcess.ExitCode -eq 0) {
             Write-Log -message "Successfully cloned mozilla-unified to $ClonePath" -severity 'INFO'
-            return $true
+            exit 0
         }
         else {
             Write-Log -message "Failed to clone mozilla-unified. Exit code: $($hgProcess.ExitCode)" -severity 'ERROR'
-            return $false
+            exit 1
         }
     }
     catch {
         Write-Log -message "Error cloning mozilla-unified: $($_.Exception.Message)" -severity 'ERROR'
-        return $false
+        exit 6
     }
 }
