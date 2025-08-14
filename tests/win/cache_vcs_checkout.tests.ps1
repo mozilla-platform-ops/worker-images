da88e4b4e7045768ac5d3c64e59a50b7e802d1b5
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2025-08-13 21:20:06 -0400

    test for revision

--- tests/win/cache_vcs_checkout.tests.ps1
+++ tests/win/cache_vcs_checkout.tests.ps1
@@ -2,7 +2,7 @@ Describe "Cache VCS Checkout" {
     It "Mozilla Unified Directory" {
         Test-Path "C:\hg-shared" | Should -Be True
     }
-    It "Mozilla Unified Contents" {
-        Test-Path "C:\hg-shared\.hg" | Should -Be True
+    It "Mozilla Unified contains revision" {
+        (Get-ChildItem "C:\hg-shared\*").Name -match '\d' | Should -Be True
     }
 }
