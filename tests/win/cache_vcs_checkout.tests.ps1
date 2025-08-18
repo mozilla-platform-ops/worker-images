245bacef7fb1d6583fee45a60dc4cf04636dac71
Jonathan Moss <jwmoss88@gmail.com>
2025-08-18 11:13:23 -0400

    fix test for vcs checkout

--- tests/win/cache_vcs_checkout.tests.ps1
+++ tests/win/cache_vcs_checkout.tests.ps1
@@ -1,8 +1,8 @@
 Describe "Cache VCS Checkout" {
     It "Mozilla Unified Directory" {
-        Test-Path "C:\hg-shared" | Should -Be True
+        Test-Path "C:\vcs-checkout" | Should -Be True
     }
     It "Mozilla Unified contains revision" {
-        (Get-ChildItem "C:\hg-shared\*").Name -match '\d' | Should -Be True
+        (Get-ChildItem "C:\vcs-checkout\*").Name | Should -Not -BeNullOrEmpty
     }
 }
