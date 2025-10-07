1c0ae69e6a98c392d1f0893b42e701261dbbe1f8
Mark Cornmesser <mcornmesser@mozilla.com>
2025-10-07 11:54:52 -0700

    add support for team param to Set-GCPWorkerImageName

--- bin/WorkerImages/Public/Set-GCPWorkerImageName.ps1
+++ bin/WorkerImages/Public/Set-GCPWorkerImageName.ps1
@@ -1,14 +1,39 @@
 function Set-GCPWorkerImageName {
     [CmdletBinding()]
     param (
-        [String]
-        $Key
+        [Parameter(Mandatory = $true)]
+        [String] $Key,
+        [Parameter(Mandatory = $false)]
+        [String] $Team
     )
     
     Set-PSRepository PSGallery -InstallationPolicy Trusted
     Install-Module powershell-yaml -ErrorAction Stop
-    $YAML = Convertfrom-Yaml (Get-Content "config/$key.yaml" -raw)
-    $ImageName = $YAML.image["image_name"]
+
+    if ($Team -and $Team -ieq "tceng") {
+        $YamlPath = "config/tceng/$Key.yaml"
+    } else {
+        $YamlPath = "config/$Key.yaml"
+    }
+
+    if (-not (Test-Path $YamlPath)) {
+        throw "YAML file not found at: $YamlPath"
+    }
+
+    $YAML = ConvertFrom-Yaml (Get-Content $YamlPath -Raw)
+
+    if ($Key -notmatch "alpha") {
+        $suffix = Get-Date -Format "yyyy-MM-dd"
+        $ImageName = -join ($YAML.image["image_name"], "-", $suffix)
+    } else {
+        $ImageName = $YAML.image["image_name"]
+    }
+
     Write-Host "Setting $ImageName as the name for the worker image"
-    Write-Output "IMAGENAME=$ImageName" >> $ENV:GITHUB_OUTPUT
+
+    if ($env:GITHUB_OUTPUT) {
+        "IMAGENAME=$ImageName" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+    } else {
+        Write-Output "IMAGENAME=$ImageName"
+    }
 }
