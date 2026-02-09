8d5665a0b87d488bbeb834dc48dc274b610d3638
Jonathan Moss <jmoss@mozilla.com>
2026-02-09 17:22:23 -0500

    fix error reporting for arm and x64 os

--- tests/win/error_reporting.tests.ps1
+++ tests/win/error_reporting.tests.ps1
@@ -1,13 +1,20 @@
-Describe "Erorr reporting" {
+Describe "Error reporting" {
     BeforeDiscovery {
         $Hiera = $Data.Hiera
     }
 
-    It "Error dump folder exists" -Skip {
-        Test-Path "D:\error-dumps" | Should -Be $True
+    BeforeAll {
+        if ((Get-CimInstance Win32_OperatingSystem).OSArchitecture -match "ARM") {
+            $DumpPath = "C:\error-dumps"
+        } else {
+            $DumpPath = "D:\error-dumps"
+        }
     }
-    It "Error dumpfolder registry exists" -Skip {
-        Get-ItemPropertyValue "HKLM:\Software\Microsoft\Windows\Windows\Error\Reporting" -Name "DumpFolder" -ErrorAction SilentlyContinue | Should -Be "D:\error-dumps"
+    It "Error dump folder exists" {
+        Test-Path $DumpPath | Should -Be $True
+    }
+    It "Error dumpfolder registry exists" {
+        Get-ItemPropertyValue "HKLM:\Software\Microsoft\Windows\Windows\Error\Reporting" -Name "DumpFolder" -ErrorAction SilentlyContinue | Should -Be $DumpPath
     }
     It "Error localdumps registry exists" {
         Get-ItemPropertyValue "HKLM:\Software\Microsoft\Windows\Windows\Error\Reporting" -Name "LocalDumps" -ErrorAction SilentlyContinue | Should -Be 1
