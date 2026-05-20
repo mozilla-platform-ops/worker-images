78cafd284d5dda604b43c41e0d4e33250665d944
Mark Cornmesser <mcornmesser@mozilla.com>
2026-05-20 15:37:09 -0700

    debugging HW deployment failures

--- provisioners/windows/MDC1Windows/OS-deploy.ps1
+++ provisioners/windows/MDC1Windows/OS-deploy.ps1
@@ -317,11 +317,15 @@ function Invoke-DownloadWithRetry {
         $attemptStartTime = Get-Date
 
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        Invoke-WebRequest -UseBasicParsing -Uri $Url -OutFile $Path
+
+        Invoke-WebRequest `
+            -UseBasicParsing `
+            -Uri $Url `
+            -OutFile $Path `
+            -Headers @{ 'User-Agent' = 'Mozilla/5.0' }
 
         $attemptSeconds = [math]::Round(($(Get-Date) - $attemptStartTime).TotalSeconds, 2)
         Write-Host "Package downloaded in $attemptSeconds seconds"
-        #Write-Log -message ('{0} :: Package downloaded in {1} seconds - {2:o}' -f $($MyInvocation.MyCommand.Name), $attemptSeconds, (Get-Date).ToUniversalTime()) -severity 'DEBUG'
         break
     }   
         catch {
