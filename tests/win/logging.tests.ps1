f9dea833f68706f2f5699e2e869b8133aa8e2cb6
jwmossmoz <jmoss@mozilla.com>
2026-09-09 18:37:38 -0400

    RELOPS-2557 Accept Papertrail bundle line endings (#920)

--- tests/win/logging.tests.ps1
+++ tests/win/logging.tests.ps1
@@ -20,8 +20,11 @@ Describe "Logging" {
     Context "Papertrail CA bundle is current" {
         It "Uses the bundle managed by Puppet" {
             $bundle = "${env:ProgramFiles(x86)}\nxlog\cert\papertrail-bundle.pem"
-            (Get-FileHash -LiteralPath $bundle -Algorithm SHA256).Hash |
-                Should -Be "AE31ECB3C6E9FF3154CB7A55F017090448F88482F0E94AC927C0C67A1F33B9CF"
+            $validHashes = @(
+                "AE31ECB3C6E9FF3154CB7A55F017090448F88482F0E94AC927C0C67A1F33B9CF" # LF
+                "DAB0D52F01E2AFC16A80ABBCADEF312CD09680639BCEB6B1BBC43BA9C7FCAEAF" # CRLF
+            )
+            $validHashes | Should -Contain (Get-FileHash -LiteralPath $bundle -Algorithm SHA256).Hash
         }
     }
 }
