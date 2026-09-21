b86c800b516e15a0f8ac8a8615a2f8c0e3150d26
Jonathan Moss <jmoss@mozilla.com>
2026-09-21 09:10:04 -0400

    Build 25H2 alpha with C drive task storage

--- tests/win/preloaded_cache_smoke.tests.ps1
+++ tests/win/preloaded_cache_smoke.tests.ps1
@@ -60,3 +60,21 @@ Describe "Preloaded pip cache" {
         $seed[0].location | Should -BeExactly 'C:\cache-seeds\pip'
     }
 }
+
+Describe "C: task storage" {
+    It "Keeps task and cache paths on the image volume" {
+        $config = Get-Content 'C:\worker-runner\runner.yml' -Raw | ConvertFrom-Yaml
+        $config.workerConfig.tasksDir | Should -BeExactly 'C:\tasks'
+        $config.workerConfig.cachesDir | Should -BeExactly 'C:\caches'
+        $config.workerConfig.downloadsDir | Should -BeExactly 'C:\downloads'
+        Get-ItemPropertyValue 'HKLM:\SOFTWARE\Mozilla\ronin_puppet' -Name task_drive | Should -BeExactly 'C:'
+        $paging = Get-ItemPropertyValue 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management' -Name PagingFiles
+        @($paging) | Should -Contain 'C:\pagefile.sys 8192 8192'
+        @($paging | Where-Object { $_ -like 'D:*' }).Count | Should -Be 0
+    }
+    It "Can start without a temporary D: volume" {
+        . 'C:\ProgramData\PuppetLabs\ronin\maintainsystem.ps1'
+        Test-AzureNvmeTemporaryDriveRequired -vmSize 'Standard_F8alds_v7' -TaskDrive 'C:' | Should -BeFalse
+        Test-AzureNvmeTemporaryDriveRequired -vmSize 'Standard_F8alds_v7' -TaskDrive 'D:' | Should -BeTrue
+    }
+}
