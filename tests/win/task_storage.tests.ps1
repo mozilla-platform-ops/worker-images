d204acd00ed7d54254a07aa9014db2f5a2cb06f8
jwmossmoz <jmoss@mozilla.com>
2026-09-22 16:15:32 -0400

    RELOPS-2583: Prepare production Windows images for C: storage (#983)
    * RELOPS-2583: Prepare production Windows images for C: storage
    
    * Remove comments from C: storage Pester test
    
    * Remove startup function unit test from image Pester checks
    
    * Rename the task storage Pester test
    
    * Bump Windows 10 production image to 1.4.0

--- tests/win/task_storage.tests.ps1
+++ tests/win/task_storage.tests.ps1
@@ -1,4 +1,3 @@
-# Selected by Windows alpha images that use C: task storage.
 Describe "C: task storage" {
     It "Configures task and cache paths on C:" {
         $config = Get-Content 'C:\worker-runner\runner.yml' -Raw | ConvertFrom-Yaml
@@ -12,14 +11,5 @@ Describe "C: task storage" {
         $paging = Get-ItemPropertyValue 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management' -Name PagingFiles
         @($paging) | Should -Contain 'C:\pagefile.sys 8192 8192'
         @($paging | Where-Object { $_ -like 'D:*' }).Count | Should -Be 0
-    }
-
-    It "Skips temporary disk initialization for C: task storage" {
-        . 'C:\ProgramData\PuppetLabs\ronin\maintainsystem.ps1'
-        Mock Write-Log {}
-        $taskDrive = Get-ItemPropertyValue 'HKLM:\SOFTWARE\Mozilla\ronin_puppet' -Name task_drive
-        # A missing setup script makes this fail if startup requires D:.
-        Ensure-AzureNvmeTemporaryDrive -vmSize 'Standard_F8alds_v7' -TaskDrive $taskDrive -scriptPath "$TestDrive\missing-disk-setup.ps1"
-        Test-AzureNvmeTemporaryDriveRequired -vmSize 'Standard_F8alds_v7' -TaskDrive 'D:' | Should -BeTrue
     }
 }
