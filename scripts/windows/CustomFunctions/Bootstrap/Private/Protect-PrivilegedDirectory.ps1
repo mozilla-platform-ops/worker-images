39e1c98d9bf1a2f9effd9ebcdef9f2b65ebab0c8
Mark Cornmesser <mcornmesser@mozilla.com>
2026-09-23 09:29:08 -0700

    RELOPS-2589 Harden Azure Windows bootstrap

---
+++ scripts/windows/CustomFunctions/Bootstrap/Private/Protect-PrivilegedDirectory.ps1
@@ -0,0 +1,27 @@
+function Protect-PrivilegedDirectory {
+    param([Parameter(Mandatory)][string] $Path)
+
+    $directorySddl = 'O:BAG:BAD:P(A;OICI;FA;;;SY)(A;OICI;FA;;;BA)'
+    $fileSddl = 'O:BAG:BAD:P(A;;FA;;;SY)(A;;FA;;;BA)'
+    $rootAcl = [System.Security.AccessControl.DirectorySecurity]::new()
+    $rootAcl.SetSecurityDescriptorSddlForm($directorySddl)
+    New-Item -ItemType Directory -Path $Path -Force -ErrorAction Stop | Out-Null
+    Set-Acl -LiteralPath $Path -AclObject $rootAcl -ErrorAction Stop
+
+    Get-ChildItem -LiteralPath $Path -Recurse -Force -ErrorAction Stop | ForEach-Object {
+        $acl = if ($_.PSIsContainer) {
+            [System.Security.AccessControl.DirectorySecurity]::new()
+        } else {
+            [System.Security.AccessControl.FileSecurity]::new()
+        }
+        $acl.SetSecurityDescriptorSddlForm($(if ($_.PSIsContainer) { $directorySddl } else { $fileSddl }))
+        Set-Acl -LiteralPath $_.FullName -AclObject $acl -ErrorAction Stop
+    }
+
+    $sections = [System.Security.AccessControl.AccessControlSections]::Access -bor
+        [System.Security.AccessControl.AccessControlSections]::Owner
+    if ((Get-Acl -LiteralPath $Path -ErrorAction Stop).GetSecurityDescriptorSddlForm($sections) -ne
+        $rootAcl.GetSecurityDescriptorSddlForm($sections)) {
+        throw "Failed to restrict $Path to SYSTEM and Administrators."
+    }
+}
--- scripts/windows/CustomFunctions/Bootstrap/Public/Set-Logging.ps1
+++ scripts/windows/CustomFunctions/Bootstrap/Public/Set-Logging.ps1
@@ -15,6 +15,8 @@ function Set-Logging {
     }
     process {
         $null = New-Item -ItemType Directory -Force -Path $local_dir -ErrorAction SilentlyContinue
+        # SECURITY: this directory contains installers executed by SYSTEM.
+        Protect-PrivilegedDirectory -Path $local_dir
         Invoke-DownloadWithRetry $ext_src/$nxlog_msi -Path $local_dir\$nxlog_msi
         #Invoke-WebRequest $ext_src/$nxlog_msi -outfile $local_dir\$nxlog_msi -UseBasicParsing
         msiexec /i $local_dir\$nxlog_msi /passive
