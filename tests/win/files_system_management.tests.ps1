eafed79137df4ec836f181ea1c534040ac72b0e6
Jonathan Moss <jmoss@mozilla.com>
2026-09-23 14:04:10 -0400

    Build Windows alpha images with builder long-path support

--- tests/win/files_system_management.tests.ps1
+++ tests/win/files_system_management.tests.ps1
@@ -9,7 +9,7 @@ Describe "File System Management" {
     It "Disable Last Acccess" {
         Get-ItemPropertyValue "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "NtfsDisableLastAccessUpdate" | Should -Be 2147483649
     }
-    It "Long paths enabled" -Skip {
+    It "Long paths enabled" {
         Get-ItemPropertyValue "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" | Should -Be 1
     }
 }
